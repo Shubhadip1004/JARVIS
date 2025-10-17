fd05ea49917e1ef15e1ec715b09446bf52c25972
Shubhadip Mahata <shubhadip.w@gmail.com>
2025-10-17 19:07:53 +0530

    Implement a check for connection to avoid crashing

--- src/news.py
+++ src/news.py
@@ -1,20 +1,13 @@
 import requests
 
-def get_news_headlines(api_key, category="technology", country="us", max_articles=5):
-    url = f"https://newsapi.org/v2/top-headlines?category={category}&country={country}&apiKey={api_key}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        articles = response.json().get("articles", [])
-        headlines = [article["title"] for article in articles[:max_articles]]
-        if not headlines:
-            return ["No news articles found."]
-        return headlines
-    else:
-        return ["Could not fetch news."]
-
-# Example usage
-
-# api_key = ""
-# headlines = get_news_headlines(api_key)
-# for idx, headline in enumerate(headlines, 1):
-#     print(f"{idx}. {headline}")
+def get_news_headlines(api_key, is_online, category="technology", country="us", max_articles=5):
+    if is_online:
+        url = f"https://newsapi.org/v2/top-headlines?category={category}&country={country}&apiKey={api_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            articles = response.json().get("articles", [])
+            headlines = [article["title"] for article in articles[:max_articles]]
+            if not headlines:
+                return ["No news articles found."]
+            return headlines
+    return ["I could not fetch news without internet connection"]
